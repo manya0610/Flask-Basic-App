05b3ffd74ffcc7369afec838f0199cad734b37df
Manish <manish@MacBook-Air.local>
2025-03-16 14:16:14 +0530

    minor changes to celery

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ alembic revision --autogenerate -m "Create a baseline migrations"
 
 To apply Migration
 alembic upgrade head
+
+
+To run celery app
+celery -A src.celery_app worker -c 1 --loglevel=INFO
--- configs/celeryconfig.py
+++ configs/celeryconfig.py
@@ -6,13 +6,12 @@ queue = RMQ_QUEUE
 exchange = RMQ_EXCHANGE
 
 routing_key = "my-routing-key"
-task_queues = (Queue(queue, Exchange(exchange), routing_key=routing_key), )
+task_queues = (Queue(queue, Exchange(exchange), routing_key=routing_key),)
 
-task_routes = {"src.celery_app.tasks.add" : {
-    "queue" : queue
-}}
+task_routes = {"src.celery_app.tasks.add": {"queue": queue},}
 
 
 worker_hijack_root_logger = False
 CELERYD_LOG_FILE = None
 worker_redirect_stdouts = False
+CELERYD_LOG_LEVEL = 'INFO'
